a5f46c8c794c795bf22783f65ab9daf65f1ca29f
adamhinton <adam.d.hinton@gmail.com>
2022-01-10 19:59:21 -0500

    added catch to delete

--- api/server.js
+++ api/server.js
@@ -5,15 +5,23 @@ const server = express();
 server.use(express.json());
 
 server.delete("/api/users/:id", async (req, res) => {
-  const possibleUser = await User.findById(req.params.id);
-  console.log("possible user:", possibleUser);
-  if (!possibleUser) {
-    res
-      .status(404)
-      .json({ message: "The user with the specified ID does not exist" });
-  } else {
-    const deletedUser = await User.remove(possibleUser.id);
-    res.status(200).json(deletedUser);
+  try {
+    const possibleUser = await User.findById(req.params.id);
+    console.log("possible user:", possibleUser);
+    if (!possibleUser) {
+      res
+        .status(404)
+        .json({ message: "The user with the specified ID does not exist" });
+    } else {
+      const deletedUser = await User.remove(possibleUser.id);
+      res.status(200).json(deletedUser);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "",
+      err: err.message,
+      stack: err.stack,
+    });
   }
 });
 
